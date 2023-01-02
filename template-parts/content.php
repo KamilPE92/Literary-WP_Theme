f985937330c063e12356b1965a7445aa54d7cabd
Kamil <kamil.marek.pietruszka@gmail.com>
2023-01-02 21:00:49 +0100

    end of 1 day

--- template-parts/content.php
+++ template-parts/content.php
@@ -5,32 +5,34 @@
 
 
                 <!-- !Card_1 start -->
-                <article class=>
-                    <div class="img--wrapper">
-                      <?php the_post_thumbnail( );?>
-                        <div class="title--art--wrapper">
-                            <p><?php the_title( ); ?></p>
-                            <!-- !title-art-wrapper end -->
-                        </div>
-                        <!-- !title-img-wrapper end -->
-                    </div>
-                    <div class="tags--wrapper">
-                        <a href="">Zdrada </a>
-                        <a href="">Megas Alexandros </a>
-                        <a href="">Megas Alexandros </a>
-                        <!-- !tags--wrapper end -->
-                    </div>
-                    <div class="excerpt--wrapper">
-                        <p>
-                        <?php the_excerpt(); ?></p>
-                    </div>
-                    <!-- !excerpt--wrapper end -->
-                    <div class="rdm--btn--wrapper">
-                        <a href="" class="rdm--btn">Czytaj</a>
-                    </div>
-                    <!-- !excerpt--wrapper end -->
+<div class="content-card">
+    
+    <div class="img--wrapper">
+          <?php the_post_thumbnail( );?>
+            <div class="title--art--wrapper">
+                <p><?php the_title( ); ?></p>
+                <!-- !title-art-wrapper end -->
+            </div>
+            <!-- !title-img-wrapper end -->
+        </div>
+        <div class="tags--wrapper">
+            <a href="">Zdrada </a>
+            <a href="">Megas Alexandros </a>
+            <a href="">Megas Alexandros </a>
+            <!-- !tags--wrapper end -->
+        </div>
+        <div class="excerpt--wrapper">
+            <p>
+            <?php the_excerpt(); ?></p>
+        </div>
+        
+        <!-- !excerpt--wrapper end -->
+        <!-- !excerpt--wrapper end -->
 
-                    <!-- !Card 1 end -->
-                </article>
+        <div class="rdm--btn--wrapper">
+            <a href="" class="read-more">Czytaj</a>
+        </div>
+        <!-- !Card 1 end -->
+</div>
 
 
